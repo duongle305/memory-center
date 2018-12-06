676e72d48d16bc1613c23ea1e0a8ca8105070fd6
Duong Le <duongrom.it.305@gmail.com>
2018-12-06 21:40:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style src="@/assets/vendors/bootstrap/css/bootstrap.min.css"></style>
+<style src="@/assets/vendors/font-awesome/css/all.css"></style>
+<style src="@/assets/css/animate.css"></style>
+<style src="@/assets/vendors/owl/css/owl.carousel.min.css"></style>
+<style src="@/assets/vendors/owl/css/owl.theme.default.min.css"></style>
+<style src="@/assets/css/index.css"></style>
